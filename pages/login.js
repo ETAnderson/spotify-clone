5ae4520d13088a722a9123905048bc7b35d18ae6
ETAnderson <anderson.eric.t@gmail.com>
2022-01-10 23:33:05 -0500

    spotify api connection
     set up nextauth with spotifyApi connection

--- pages/login.js
+++ pages/login.js
@@ -2,10 +2,23 @@ import { getProviders, signIn } from 'next-auth/react';
 
 function Login({ providers }) {
     return (
-        <div>
-            <h1>This is the login page</h1>
+        <div className='flex flex-col items-center bg-black min-h-screen w-full justify-center'>
+            <img 
+                className='w-52 mb-5'
+                src="https://upload.wikimedia.org/wikipedia/commons/2/26/Spotify_logo_with_text.svg" 
+                alt="for demonstration purposes" 
+            />
+
+        {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+                <button 
+                    onClick={() => signIn(provider.id, { callbackUrl: '/'})}
+                    className='bg-[#18D860]  text-white p-5 rounded-full'>Login with {provider.name}</button>
+                </div>
+        ))}
+
         </div>
-    )
+    );
 }
 
 export default Login;
